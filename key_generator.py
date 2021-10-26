3491e7676f166b154f0757f9075e15221db2dc86
Cristian Espinosa <cristianespinosacaer@gmail.com>
2021-10-26 18:55:07 -0500

    key generator v1.0 done

---
+++ key_generator.py
@@ -0,0 +1,30 @@
+import random
+class KeyGenerator:
+    abc_lowercase=tuple(letter for letter in "abcdefghijklmnopqrstuvwxyz")
+    abc_uppercase=tuple(letter.capitalize() for letter in abc_lowercase)
+    numbers=tuple(str(i) for i in range(0,10))
+    symbols=tuple(symbol for symbol in "@/_-*+$&%#")
+    available_charecters=abc_lowercase+abc_uppercase+numbers+symbols
+    
+    def maker(self,length_key):
+        key=''
+        for i in range(length_key):
+            key+=random.choice(self.available_charecters)
+        
+        return key
+
+    def __str__(self) -> str:
+        return f'available characters: \n {self.available_charecters}'
+
+
+
+
+def run():
+    key_gen=KeyGenerator()
+    print(key_gen)
+    key=key_gen.maker(5)
+    print('new key: ')
+    print(key)
+
+if __name__=='__main__':
+    run()
