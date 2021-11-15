a7a6036b9ccd6b2b3def2ec84752016b6053aee2
Cristian Espinosa <cristianespinosacaer@gmail.com>
2021-11-15 16:21:58 -0500

    set operations

---
+++ sets.py
@@ -0,0 +1,9 @@
+set1={1,2,3,4}
+set2={3,4,5,6}
+
+print('union operation', set1 |set2)
+print('interception',set1 & set2)
+print('difference Set1 -Set2',set1-set2)
+print('difference Set2 -Set1',set2-set1)
+print('symmetric difference', set1 ^set2)
+
